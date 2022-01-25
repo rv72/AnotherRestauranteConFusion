64332c3316f161717faac309d0626cda16b5ade4
Reuben Varghese <rjvarghe@purdue.edu>
2022-01-25 18:34:02 -0500

    setup

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Reach, { Component } from 'react';
+import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap'
+import Menu from './components/MenuComponent'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Navbar dark color="primary">
+          <div className='container'>
+            <NavbarBrand href="/">Restaurante Con Fusion</NavbarBrand>
+          </div>
+      </Navbar>
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
