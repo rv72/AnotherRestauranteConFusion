59d9cd44e06a5ea39499837f01fe4fe85a65a6ce
Reuben Varghese <rjvarghe@purdue.edu>
2022-02-19 16:15:03 -0500

    React Router URL parameters

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
-
+import { Card, CardImg, CardText, CardBody, CardTitle , Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderDish({ dish }) {
     if (dish != null)
@@ -51,19 +51,22 @@ function RenderComments({ comments }) {
 
 }
 
-function CheckComments({ selectedDish }) {
-
-    if (selectedDish != null)
-        return (<RenderComments comments={selectedDish.comments} />);
-    else
-        return (<div></div>);
-
-}
-
 const DishDetail = (props) => {
 
     return (
         <div className="container">
+
+            <div className='row'>
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className='col-12'>
+                    <h3>{props.dish.name}</h3>
+                    <hr/>
+                </div>
+            </div>
+
             <div className="row">
 
                 <div className="col-12 col-md-5 m-1">
@@ -71,7 +74,7 @@ const DishDetail = (props) => {
                 </div>
 
                 <div className="col-12 col-md-5 m-1">
-                    <CheckComments selectedDish={props.dish} />
+                    <RenderComments comments={props.comments} />
                 </div>
 
             </div>
